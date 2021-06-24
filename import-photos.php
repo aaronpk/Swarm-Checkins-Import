4e8bb689ba588545a001338314bc766992c22b57
Aaron Parecki <aaron@parecki.com>
2021-06-24 10:59:52 -0700

    add photo import script

---
+++ import-photos.php
@@ -0,0 +1,46 @@
+<?php
+require('vendor/autoload.php');
+
+$checkins = glob('checkins/*/*/*/*.json');
+
+foreach($checkins as $filename) {
+
+  // if($filename != 'checkins/2016/04/04/095557.json')
+  //   continue;
+
+  $checkin = json_decode(file_get_contents($filename), true);
+  #print_r($checkin);
+
+  if(isset($checkin['photos']['items'])) {
+    foreach($checkin['photos']['items'] as $photo) {
+
+      process_photo($filename, $checkin, $photo);
+
+    }
+  }
+
+}
+
+
+function process_photo($filename, $checkin, $photo) {
+
+  $folder = dirname($filename);
+  $photo_filename = $folder . '/' . $photo['id'] . '.jpg';
+
+  echo $folder."\n";
+  echo $photo['suffix']."\n";
+
+  $url = $photo['prefix'] . $photo['width'].'x'.$photo['height'] . $photo['suffix'];
+  echo $url."\n";
+
+
+  $fp = fopen($photo_filename, 'w');
+
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_FILE, $fp);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+  curl_exec($ch);
+  curl_close($ch);
+
+  echo "Saved to $photo_filename\n";
+}
